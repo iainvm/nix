4f2c39a00e99d179d5704a5033701b6aed5fb607
Iain Majer <iainvm@outlook.com>
2024-10-19 19:29:57 +0100

    Refactor plymouth, set wofi font

--- nixosModules/system/plymouth.nix
+++ nixosModules/system/plymouth.nix
@@ -9,33 +9,48 @@
 in {
   options.plymouth = {
     enable = lib.mkEnableOption "enable plymouth";
+    silent-boot = lib.mkEnableOption "enable silent boot";
   };
 
-  config = lib.mkIf config.plymouth.enable {
-    boot.initrd.systemd.enable = true;
-    boot.plymouth = {
-      enable = true;
-      theme = "hexagon_2";
-      themePackages = [
-        pkgs.adi1090x-plymouth-themes
-      ];
-    };
+  config = lib.mkMerge [
+    {}
 
-    # # Enable "Silent Boot"
-    # consoleLogLevel = 0;
-    # initrd.verbose = false;
-    # kernelParams = [
-    #   "quiet"
-    #   "splash"
-    #   "boot.shell_on_fail"
-    #   "loglevel=3"
-    #   "rd.systemd.show_status=false"
-    #   "rd.udev.log_level=3"
-    #   "udev.log_priority=3"
-    # ];
-    # # Hide the OS choice for bootloaders.
-    # # It's still possible to open the bootloader list by pressing any key
-    # # It will just not appear on screen unless a key is pressed
-    # loader.timeout = 0;
-  };
+    (
+      lib.mkIf config.plymouth.enable
+      {
+        boot = {
+          initrd.verbose = false;
+          plymouth = {
+            enable = true;
+            theme = "hexagon_2";
+            themePackages = [
+              pkgs.adi1090x-plymouth-themes
+            ];
+          };
+        };
+      }
+    )
+
+    (lib.mkIf config.plymouth.silent-boot
+      {
+        boot = {
+          consoleLogLevel = 0;
+          initrd.verbose = false;
+          initrd.systemd.enable = true;
+          kernelParams = [
+            "quiet"
+            "splash"
+            "boot.shell_on_fail"
+            "loglevel=3"
+            "rd.systemd.show_status=false"
+            "rd.udev.log_level=3"
+            "udev.log_priority=3"
+          ];
+          # Hide the OS choice for bootloaders.
+          # It's still possible to open the bootloader list by pressing any key
+          # It will just not appear on screen unless a key is pressed
+          loader.timeout = 0;
+        };
+      })
+  ];
 }
