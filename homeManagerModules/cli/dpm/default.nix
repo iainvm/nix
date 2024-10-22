130d50ec2623b35d03f02108d39d36ed6fdbd2ab
Iain Majer <iainvm@outlook.com>
2024-10-22 18:11:54 +0100

    feat: add completions to DPM

--- homeManagerModules/cli/dpm/default.nix
+++ homeManagerModules/cli/dpm/default.nix
@@ -14,6 +14,13 @@
       sha256 = "sha256-9xpscgu/tHyvStJpqlHx31Nn4Gqj7wRLCNDOQQd8nIM=";
     };
     vendorHash = "sha256-kV98peQAzCmRaVyMgotoyNNjU74dZKPpMU8wKvXd8bU=";
+    nativeBuildInputs = [ pkgs.installShellFiles ];
+    postInstall = ''
+    installShellCompletion --cmd dpm \
+      --bash <($out/bin/dpm completion bash) \
+      --fish <($out/bin/dpm completion fish) \
+      --zsh <($out/bin/dpm completion zsh)
+    '';
   };
 in {
   options.cli.dpm = {
