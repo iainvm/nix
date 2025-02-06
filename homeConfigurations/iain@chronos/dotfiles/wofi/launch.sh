a2440c4e09227c89e3d466b3d7ce33e6861abdc7
Iain Majer <iain.majer@river-island.com>
2025-02-06 14:05:11 +0000

    refactor: moved common wofi scripts into wofi module

--- homeConfigurations/iain@chronos/dotfiles/wofi/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-wofi --allow-images --show drun
--- homeModules/applications/wofi/default.nix
+++ homeModules/applications/wofi/default.nix
@@ -8,6 +8,14 @@
   };
 
   config = lib.mkIf config.applications.wofi.enable {
+    home.file = {
+      ".config/wofi" = {
+        source = ./dotfiles;
+        target = ".config/wofi/";
+        recursive = true;
+      };
+    };
+
     programs.wofi = {
       enable = true;
     };
