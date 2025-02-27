fb6006862fca87627c8c84a892fb4ee42b5c8a5d
Iain Majer <iain.majer@river-island.com>
2025-02-27 11:49:40 +0000

    fix: anytype frozen to PR branch

--- homeModules/applications/anytype/default.nix
+++ homeModules/applications/anytype/default.nix
@@ -4,11 +4,20 @@
   config,
   ...
 }: let
+  pkgs_anytype = import (pkgs.fetchFromGitHub {
+    owner = "TomaSajt";
+    repo = "nixpkgs";
+    rev = "anytype";
+    sha256 = "sha256-a0Aawv0FXZvdQRsO4sPysrRArvJajtxKNdapZ29KAfU=";
+  }) {
+    inherit (pkgs) system;
+    config.allowUnfree = true;
+  };
   # Wrap the binary to ensure different users have different tmp files
   # Without this I was having problems with `tmp/go-graphviz` and 1 user owning the folder
   wrapped = pkgs.symlinkJoin {
     name = "anytype";
-    paths = [pkgs.anytype];
+    paths = [pkgs_anytype.anytype];
     buildInputs = [pkgs.makeWrapper];
     postBuild = ''
       wrapProgram $out/bin/anytype --run 'export TMP="/tmp/$USER-anytype"' --run  'export TMPDIR="/tmp/$USER-anytype"'
