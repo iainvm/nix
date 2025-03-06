dd16d968ee54a9db6d9a6dfb7811fe3911705e11
Iain Majer <iain.majer@river-island.com>
2025-03-06 12:36:38 +0000

    feat: rivpn now has a small cli

--- homeModules/network/ri-vpn/default.nix
+++ homeModules/network/ri-vpn/default.nix
@@ -20,5 +20,24 @@
         ExecStart = "/run/wrappers/bin/sudo gpclient --fix-openssl connect vpn.river-island.com";
       };
     };
+
+    home.packages = with pkgs; [
+      (writeScriptBin "rivpn" ''
+        case "$1" in
+          start)
+            systemctl --user start ri-vpn.service
+            ;;
+          stop)
+            systemctl --user stop ri-vpn.service
+            ;;
+          status)
+            systemctl --user status ri-vpn.service
+            ;;
+          *)
+            echo "Usage: rivpn {start|stop|status}"
+            ;;
+        esac
+      '')
+    ];
   };
 }
