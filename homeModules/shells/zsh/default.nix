fa9acb4f835faf0f79e45733ae82a38446cfbd37
Iain Majer <iain.majer@river-island.com>
2025-07-23 12:34:05 +0100

    feat: added gpr command to open github prs

--- homeModules/shells/zsh/default.nix
+++ homeModules/shells/zsh/default.nix
@@ -95,6 +95,7 @@ in {
               grb = "git rebase";
               gst = "git stash";
               gpuoc = "git push --set-upstream origin \$(git symbolic-ref --short HEAD)";
+              gpr = ''xdg-open "https://github.com/$(git remote get-url origin | sed -E 's/git@github.com:(.+)\.git/\1/')/compare/$(git rev-parse --abbrev-ref HEAD)"'';
               gf = "git fetch --all --prune";
               gd = "git diff";
               la = "ls -la";
