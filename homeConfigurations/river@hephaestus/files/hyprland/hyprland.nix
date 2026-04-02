581a8d0832ea3fa1b04b60c4ade5ef0fad931431
Iain Majer <iain.majer@river-island.com>
2026-04-03 00:08:49 +0100

    Added bindings for monitor brightness

--- homeConfigurations/river@hephaestus/files/hyprland/hyprland.nix
+++ homeConfigurations/river@hephaestus/files/hyprland/hyprland.nix
@@ -17,18 +17,20 @@
     "$mainMod, l, exec, sleep 1 && hyprctl -i 0 dispatch dpms standby"
     ", Print, exec, $configs/swappy/screenshot.sh"
     "SHIFT, Print, exec, $configs/swappy/edit-clipboard.sh"
+    ",XF86MonBrightnessUp,exec,brightnessctl set 10%+"
+    ",XF86MonBrightnessDown,exec,brightnessctl set 10%-"
   ];
 
   monitor = [
     "desc:LG Electronics 23MP65 0x0006E8D2,1920x1080@60,0x0,1"
     "desc:GIGA-BYTE TECHNOLOGY CO. LTD. G34WQC A 21512B002947,3440x1440@100,1920x0,1"
-    #"desc:Sharp Corporation 0x14D0,1920x1200@60,5360x0,1"
+    "desc:BOE NE135A1M-NY1,2880x1920@120,5360x0,1"
   ];
 
   workspace = [
     "1, monitor:desc:LG Electronics 23MP65 0x0006E8D2, default:true"
     "2, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. G34WQC A 21512B002947, default:true"
-    "3, monitor:desc:Sharp Corporation 0x14D0, default:true"
+    "3, monitor:desc:BOE NE135A1M-NY1, default:true"
   ];
 
   windowrule = [
--- homeConfigurations/river@hephaestus/home.nix
+++ homeConfigurations/river@hephaestus/home.nix
@@ -29,6 +29,7 @@ in {
   home.packages = with pkgs; [
     postman
     rustdesk-flutter
+    brightnessctl
   ];
 
   system = {
